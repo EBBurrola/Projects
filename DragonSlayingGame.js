f77ec78ef402c572cebbc4a3f645669268b3b35d
Ericka Burrola <ericka.burrola@gmail.com>
2016-06-20 18:09:48 -0400

    added dragon slaying game and npm Basics course from Treehouse

---
+++ DragonSlayingGame.js
@@ -0,0 +1,28 @@
+/*
+
+Dragon Slaying Game from Codecademy
+///////////////////////////////////////////
+Make changes, add things, personalize
+
+*/
+
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+var totalDamage = 0;
+
+while(slaying) {
+    if (youHit) {
+        console.log("Congratulations! You hit the dragon!");
+        totalDamage += damageThisRound;
+        if (totalDamage >= 4) {
+            console.log("You have killed the dragon!");
+            slaying = false;
+        } else {
+            youHit = Math.floor(Math.random() * 2);
+        };
+    } else {
+        console.log("The dragon defeated you!");
+    };
+    slaying = false;
+};
